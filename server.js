0d1899be503bb98682ba5b2aa6cb0e31e9097343
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-04 12:55:41 +0300

    Initialize db
    Initialize database service account and database

--- server.js
+++ server.js
@@ -4,3 +4,12 @@ const admin = require('firebase-admin');
 const bcrypt = require('bcrypt');
 const path = require('path');
 const nodemailer = require('nodemailer');
+
+// firebase admin setup
+let serviceAccount = require(path.join(__dirname,"clothing-site-b85f5-firebase-adminsdk-krhrz-6e08f768f3"));
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+let db = admin.firestore();
