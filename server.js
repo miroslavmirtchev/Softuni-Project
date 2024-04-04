ab1751000dd5cfc28a8887fbaf3e43e8c566a821
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-04 12:54:54 +0300

    Update server
    Import packages in server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+// importing packages
+const express = require('express');
+const admin = require('firebase-admin');
+const bcrypt = require('bcrypt');
+const path = require('path');
+const nodemailer = require('nodemailer');
