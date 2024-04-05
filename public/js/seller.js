b0ae9261dc0e1de49e28755322181663fac4a80d
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-06 01:16:38 +0300

    Create onload function
    This function checks if the user is a seller, and if he is not, sends him back to login, if he is, he can access adding products

--- public/js/seller.js
+++ public/js/seller.js
@@ -5,3 +5,20 @@ const becomeSellerElement = document.querySelector('.become-seller');
 const productListingElement = document.querySelector('.product-listing');
 const applyForm = document.querySelector('.apply-form');
 const showApplyFormBtn = document.querySelector('#apply-btn');
+
+window.onload = () => {
+    if(user){
+        if(compareToken(user.authToken, user.email)){
+            if(!user.seller){
+                becomeSellerElement.classList.remove('hide');
+            } else{
+                loader.style.display = 'block';
+                setupProducts();
+            }
+        } else{
+            location.replace('/login');
+        }
+    } else{
+        location.replace('/login');
+    }
+}
