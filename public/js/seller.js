6c8588c6a0b50604bbdf567c9ed7cf4259ee14a1
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-06 01:15:04 +0300

    Create variables
    Create variables that use the buttons in the page

--- public/js/seller.js
+++ public/js/seller.js
@@ -1,2 +1,7 @@
 let loader = document.querySelector('.loader');
 let user = JSON.parse(sessionStorage.user || null);
+
+const becomeSellerElement = document.querySelector('.become-seller');
+const productListingElement = document.querySelector('.product-listing');
+const applyForm = document.querySelector('.apply-form');
+const showApplyFormBtn = document.querySelector('#apply-btn');
