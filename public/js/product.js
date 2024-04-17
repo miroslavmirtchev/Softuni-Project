76654bba8d5aecd6004475879835414a522c6369
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-17 08:23:43 +0300

    Add toggle button sizes function

--- public/js/product.js
+++ public/js/product.js
@@ -11,3 +11,18 @@ productImages.forEach((item, i) => { // looping through each image thumb
         activeImageSlide = i; // updating the image slider variable to track current thumb
     })
 })
+
+// toggle size buttons
+
+const sizeBtns = document.querySelectorAll('.size-radio-btn'); // selecting size buttons
+let checkedBtn = 0; // current selected button
+let size;
+
+sizeBtns.forEach((item, i) => { // looping through each button
+    item.addEventListener('click', () => { // adding click event to each 
+        sizeBtns[checkedBtn].classList.remove('check'); // removing check class from the current button
+        item.classList.add('check'); // adding check class to clicked button
+        checkedBtn = i; // upading the variable
+        size = item.innerHTML;
+    })
+})
