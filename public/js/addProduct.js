d25725b5cd15286fdea79b9a6c29e3f2995b39e5
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-18 20:21:06 +0300

    Creating variables and price input functions

--- public/js/addProduct.js
+++ public/js/addProduct.js
@@ -11,3 +11,23 @@ window.onload = () => {
         location.replace('/login');
     }
 }
+
+// price inputs
+
+const actualPrice = document.querySelector('#actual-price');
+const discountPercentage = document.querySelector('#discount');
+const sellingPrice = document.querySelector('#sell-price');
+
+discountPercentage.addEventListener('input', () => {
+    if(discountPercentage.value > 100){
+        discountPercentage.value = 90;
+    } else{
+        let discount = actualPrice.value * discountPercentage.value / 100;
+        sellingPrice.value = actualPrice.value - discount;
+    }
+})
+
+sellingPrice.addEventListener('input', () => {
+    let discount = (sellingPrice.value / actualPrice.value) * 100;
+    discountPercentage.value = discount;
+})
