762eb6dfbfa98315636c069ae8d6bf883d50af77
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-18 19:00:07 +0300

    Create legit checks for the form

--- public/js/seller.js
+++ public/js/seller.js
@@ -37,3 +37,23 @@ const about = document.querySelector('#about');
 const number = document.querySelector('#number');
 const tac = document.querySelector('#terms-and-cond');
 const legitInfo = document.querySelector('#legitInfo');
+
+applyFormButton.addEventListener('click', () => {
+    if(!businessName.value.length || !address.value.length || !about.value.length || !number.value.length){
+        showAlert('fill all the inputs');
+    } else if(!tac.checked || !legitInfo.checked){
+        showAlert('you must agree to our terms and conditions');
+    } else{
+        // making server request
+        loader.style.display = 'block';
+        sendData('/seller', {
+            name: businessName.value,
+            address: address.value,
+            about: about.value,
+            number: number.value,
+            tac: tac.checked,
+            legit: legitInfo.checked,
+            email: JSON.parse(sessionStorage.user).email
+        })
+    }
+})
