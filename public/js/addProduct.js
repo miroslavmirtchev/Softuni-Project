c12f983588240a7cf29b7d265dce18e2d9b05fb8
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-18 20:26:10 +0300

    Fetch products to server and db

--- public/js/addProduct.js
+++ public/js/addProduct.js
@@ -194,3 +194,18 @@ const setFormsData = (data) => {
         }
     })
 }
+
+const fetchProductData = () => {
+    fetch('/get-products', {
+        method: 'post',
+        headers: new Headers({'Content-Type': 'application/json'}),
+        body: JSON.stringify({email: user.email, id: productId})
+    })
+    .then((res) => res.json())
+    .then(data => {
+        setFormsData(data);
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
