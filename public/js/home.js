ef081693e2034c4a211541b9e7910c2cd05bfc4e
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-18 19:04:47 +0300

    Create add products to cart function

--- public/js/home.js
+++ public/js/home.js
@@ -76,3 +76,23 @@ const createProductCards = (data, parent) => {
         return start + middle + end;
     }
 }
+
+const add_product_to_cart_or_wishlist = (type, product) => {
+    let data = JSON.parse(localStorage.getItem(type));
+    if(data == null){
+        data = [];
+    }
+
+    product = {
+        item: 1,
+        name: product.name,
+        sellPrice: product.sellPrice,
+        size: size || null,
+        shortDes: product.shortDes,
+        image: product.images[0]
+    }
+
+    data.push(product);
+    localStorage.setItem(type, JSON.stringify(data));
+    return 'added';
+}
