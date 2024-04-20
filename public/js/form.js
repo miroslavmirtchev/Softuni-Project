9b0cd983ae222146e2caef7271c8e3246d400515
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-20 10:27:14 +0300

    Create redirect

---
+++ public/js/form.js
@@ -0,0 +1,9 @@
+// redirect to home page if user logged in
+window.onload = () => {
+    if(sessionStorage.user){
+        user = JSON.parse(sessionStorage.user);
+        if(compareToken(user.authToken, user.email)){
+            location.replace('/');
+        }
+    }
+}
