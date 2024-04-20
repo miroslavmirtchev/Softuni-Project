7e435c6b7d7046fd0bfaaeca4987a1b7a0c381bc
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-20 10:32:26 +0300

    Add an address form

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -28,3 +28,19 @@ placeOrderBtn.addEventListener('click', () => {
         })
     }
 })
+
+const getAddress = () => {
+    // validation
+    let address = document.querySelector('#address').value;
+    let street = document.querySelector('#street').value;
+    let city = document.querySelector('#city').value;
+    let state = document.querySelector('#state').value;
+    let pincode = document.querySelector('#pincode').value;
+    let landmark = document.querySelector('#landmark').value;
+
+    if(!address.length || !street.length || !city.length || !state.length || !pincode.length ||  !landmark.length){
+        return showAlert('fill all the inputs first');
+    } else{
+        return { address, street, city, state, pincode, landmark };
+    }
+}
