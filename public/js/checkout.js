df743456a259a726d8ee5b58b0707fbe3611765c
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-20 10:32:04 +0300

    Create button
    Create the place order button and send the order to db when clicked

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -3,3 +3,28 @@ window.onload = () => {
         location.replace('/login');
     }
 }
+
+const placeOrderBtn = document.querySelector('.place-order-btn');
+placeOrderBtn.addEventListener('click', () => {
+    let address = getAddress();
+
+    if(address){
+        fetch('/order', {
+            method: 'post',
+            headers: new Headers({'Content-Type': 'application/json'}),
+            body: JSON.stringify({
+                order: JSON.parse(localStorage.cart),
+                email: JSON.parse(sessionStorage.user).email,
+                add: address,
+            })
+        }).then(res => res.json())
+        .then(data => {
+            if(data.alert == 'your order is placed'){
+                delete localStorage.cart;
+                showAlert(data.alert, 'success');
+            } else{
+                showAlert(data.alert);
+            }
+        })
+    }
+})
