b67be53e43752a73a513a9e0e0b62ac063f57f48
Miroslav Mirtchev <mirtchevmirko@gmail.com>
2024-04-23 19:26:42 +0300

    Add adding to cart

--- public/js/cart.js
+++ public/js/cart.js
@@ -42,3 +42,50 @@ const updateBill = () => {
     let billPrice = document.querySelector('.bill');
     billPrice.innerHTML = `$${totalBill}`;
 }
+
+const setupEvents = (name) => {
+    // setup counter event
+    const counterMinus = document.querySelectorAll(`.${name} .decrement`);
+    const counterPlus = document.querySelectorAll(`.${name} .increment`);
+    const counts = document.querySelectorAll(`.${name} .item-count`);
+    const price = document.querySelectorAll(`.${name} .sm-price`);
+    const deleteBtn = document.querySelectorAll(`.${name} .sm-delete-btn`);
+
+    let product = JSON.parse(localStorage.getItem(name));
+
+    counts.forEach((item, i) => {
+        let cost = Number(price[i].getAttribute('data-price'));
+
+        counterMinus[i].addEventListener('click', () => {
+            if(item.innerHTML > 1){
+                item.innerHTML--;
+                totalBill -= cost;
+                price[i].innerHTML = `$${item.innerHTML * cost}`;
+                if(name == 'cart'){ updateBill() }                
+                product[i].item = item.innerHTML;
+                localStorage.setItem(name, JSON.stringify(product));
+            }
+        })
+        counterPlus[i].addEventListener('click', () => {
+            if(item.innerHTML < 9){
+                item.innerHTML++;
+                totalBill += cost;
+                price[i].innerHTML = `$${item.innerHTML * cost}`                
+                if(name == 'cart'){ updateBill() } 
+                product[i].item = item.innerHTML;
+                localStorage.setItem(name, JSON.stringify(product));
+            }
+        })
+    })
+
+    deleteBtn.forEach((item, i) => {
+        item.addEventListener('click', () => {
+            product = product.filter((data, index) =>  index != i);
+            localStorage.setItem(name, JSON.stringify(product));
+            location.reload();
+        })
+    })
+}
+
+setProducts('cart');
+setProducts('wishlist');
